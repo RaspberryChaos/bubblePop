97fe70eb5de72e51c982d19291192f13006dc8f8
Ailie McCorkindale <ailiem134@hotmail.com>
2021-02-12 13:22:21 +0100

    bubbles added

--- app.js
+++ app.js
@@ -73,14 +73,61 @@ class Player {
 
 const player = new Player();
 
-
 // Bubbles
+
+const bubblesArray = [];
+
+class Bubble {
+    constructor() {
+        this.x = Math.random() * canvas.width;
+        this.y = canvas.height + 100;
+        this.radius = 50;
+        this.speed = Math.random() * 5 + 1;
+        this.distance;
+    }
+
+    update() {
+        this.y -= this.speed;
+    }
+
+    draw() {
+        ctx.fillStyle = "blue";
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.fill();
+        ctx.closePath();
+        ctx.stroke();
+    }
+}
+
+function handleBubbles() {
+    if(gameFrame % 50 === 0) {
+        bubblesArray.push(new Bubble());
+    }
+
+    for(let i = 0; i < bubblesArray.length; i++) {
+        bubblesArray[i].update();
+        bubblesArray[i].draw();
+    }
+
+    for(let i = 0; i < bubblesArray.length; i++) {
+        if(bubblesArray[i].y < 0 - this.radius*2) {
+            bubblesArray.splice(i,1);
+        }
+    }
+}
+
+
 // Animation Loop
 
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    handleBubbles();
     player.update();
     player.draw();
+    ctx.fillStyle = "black";
+    ctx.fillText(`Score: ${score}`, 10, 50);
+    gameFrame++;
     requestAnimationFrame(animate);
 }
 

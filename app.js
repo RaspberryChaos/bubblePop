62e9b845c20b3a24e9fe7f8dc83d8327e79a1fba
Ailie McCorkindale <ailiem134@hotmail.com>
2021-02-12 13:37:35 +0100

    collision detection and score update added

--- app.js
+++ app.js
@@ -84,10 +84,14 @@ class Bubble {
         this.radius = 50;
         this.speed = Math.random() * 5 + 1;
         this.distance;
+        this.counted = false;
     }
 
     update() {
         this.y -= this.speed;
+        const dx = this.x - player.x;
+        const dy = this.y - player.y;
+        this.distance = Math.sqrt(dx*dx + dy*dy);
     }
 
     draw() {
@@ -114,7 +118,16 @@ function handleBubbles() {
         if(bubblesArray[i].y < 0 - this.radius*2) {
             bubblesArray.splice(i,1);
         }
+        if(bubblesArray[i].distance < bubblesArray[i].radius + player.radius) {
+            console.log("collision");
+            if(!bubblesArray[i].counted) {
+                score++;
+                bubblesArray[i].counted = true;
+                bubblesArray.splice(i,1);
+            }
+        }
     }
+
 }
 
 

8a6df8b4d6dd6da8f0df0b8242e539d2cb27d7a5
Ailie McCorkindale <ailiem134@hotmail.com>
2021-02-12 20:36:08 +0100

    fish images and bubble pop sound effect added

--- app.js
+++ app.js
@@ -31,12 +31,17 @@ canvas.addEventListener("mouseup", function() {
 
 // Player
 
+const playerLeft = new Image();
+playerLeft.src = 'fish_swim_left.png';
+const playerRight = new Image();
+playerRight.src = 'fish_swim_right.png';
+
 class Player {
     constructor() {
         this.x = canvas.width/2;
         this.y = canvas.height/2;
         this.radius = 50;
-        this.angle = 0;
+        this.angle = 20;
         this.frameX = 0;
         this.frameY = 0;
         this.frame = 0;
@@ -47,6 +52,8 @@ class Player {
     update() {
         const dx = this.x - mouse.x;
         const dy = this.y - mouse.y;
+        let theta = Math.atan2(dy, dx);
+        this.angle = theta;
         if(mouse.x !== this.x) {
             this.x -= dx/20;
         };
@@ -56,18 +63,24 @@ class Player {
     }
 
     draw() {
-        if(mouse.click) {
+     /*   if(mouse.click) {
             ctx.linewidth = 0.2;
             ctx.beginPath();
             ctx.moveTo(this.x, this.y);
             ctx.lineTo(mouse.x, mouse.y);
             ctx.stroke();
-        }
+        }*/
         ctx.fillStyle = "red";
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2);
         ctx.fill();
         ctx.closePath();
+        //ctx.fillRect(this.x, this.y,this.radius,10);
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.angle);
+        ctx.drawImage((this.x >= mouse.x ? playerLeft : playerRight), this.frameX * this.spriteWidth, this.frameY * this.spriteHeight, this.spriteWidth, this.spriteHeight, 0 -60, 0 -45, this.spriteWidth/4, this.spriteHeight/4)
+        ctx.restore();
     }
 }
 
@@ -85,6 +98,7 @@ class Bubble {
         this.speed = Math.random() * 5 + 1;
         this.distance;
         this.counted = false;
+        this.sound = Math.random() <= 0.5 ? "sound1" : "sound2";
     }
 
     update() {
@@ -104,6 +118,11 @@ class Bubble {
     }
 }
 
+const bubblePop1 = document.createElement("audio");
+bubblePop1.src = "pop1.ogg";
+const bubblePop2 = document.createElement("audio");
+bubblePop2.src = "pop2.ogg";
+
 function handleBubbles() {
     if(gameFrame % 50 === 0) {
         bubblesArray.push(new Bubble());
@@ -118,12 +137,21 @@ function handleBubbles() {
         if(bubblesArray[i].y < 0 - this.radius*2) {
             bubblesArray.splice(i,1);
         }
-        if(bubblesArray[i].distance < bubblesArray[i].radius + player.radius) {
-            console.log("collision");
-            if(!bubblesArray[i].counted) {
-                score++;
-                bubblesArray[i].counted = true;
-                bubblesArray.splice(i,1);
+        if(bubblesArray[i]) {
+            if(bubblesArray[i].distance < bubblesArray[i].radius + player.radius) {
+                console.log("collision");
+                if(!bubblesArray[i].counted) {
+                    if(bubblesArray[i].sound === "sound1") {
+                        bubblePop1.play();
+                        console.log(1)
+                    } else {
+                        bubblePop2.play();
+                        console.log(2)
+                    }
+                    score++;
+                    bubblesArray[i].counted = true;
+                    bubblesArray.splice(i,1);
+                }
             }
         }
     }
